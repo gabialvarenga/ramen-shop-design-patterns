0b29234eebcb7049c9b3c6a5643dd5c4877dbfc2
Gabriela Alvarenga <160603188+gabialvarenga@users.noreply.github.com>
2024-11-25 19:53:20 -0300

    Finalização do main

--- ramen_shop/src/main/java/br/lpm/main/Main.java
+++ ramen_shop/src/main/java/br/lpm/main/Main.java
@@ -4,16 +4,12 @@ import br.lpm.core.BalancoFinal;
 import br.lpm.core.Pedido;
 import br.lpm.factories.RamenFactory;
 import br.lpm.singletons.PedidosSingleton;
-import br.lpm.types.AcrescimoChilli;
-import br.lpm.types.AcrescimoCremeAlho;
-import br.lpm.types.AcrescimoCroutons;
-import br.lpm.types.AcrescimoShitake;
-import br.lpm.types.AcrescimoTofu;
-import br.lpm.types.AcrescimoProteinaExtra;
+import br.lpm.types.*;
 
 public class Main {
     public static void main(String[] args) {
 
+   
         PedidosSingleton pedidos = PedidosSingleton.getInstance();
 
         // Cria os pedidos
@@ -21,16 +17,19 @@ public class Main {
         pedido1 = new AcrescimoCremeAlho(pedido1);
         pedido1 = new AcrescimoTofu(pedido1);
         pedido1 = new AcrescimoCroutons(pedido1);
+        pedido1 = new AcrescimoRefrigerante(pedido1);
 
         Pedido pedido2 = RamenFactory.criarRamen("grande", "Porco");
         pedido2 = new AcrescimoCremeAlho(pedido2);
         pedido2 = new AcrescimoChilli(pedido2);
         pedido2 = new AcrescimoProteinaExtra(pedido2);
+        pedido2 = new AcrescimoOcha(pedido2);
 
         Pedido pedido3 = RamenFactory.criarRamen("pequeno", "Vegano");
         pedido3 = new AcrescimoShitake(pedido3);
+        pedido3 = new AcrescimoKocha(pedido3);
 
-        // Adiciona os pedidos
+        // Adiciona os pedidos ao sistema
         System.out.println("\n=== Pedidos Adicionados ===");
         pedidos.adicionarPedido(pedido1);
         pedidos.adicionarPedido(pedido2);
@@ -39,15 +38,15 @@ public class Main {
         // Exibe a fila de pedidos
         pedidos.exibirFila();
 
-        // Processa o primeiro pedido
+        // Processa os pedidos
         pedidos.processarProximoPedido();
         pedidos.processarProximoPedido();
         pedidos.processarProximoPedido();
 
-        // Marca como retirado e exibe o balanço
+        // Marca o primeiro pedido como retirado
         pedidos.marcarPedidoComoRetirado(pedido1);
 
-        // Exibe o balanço final
+        // Exibe o balanço final do restaurante
         BalancoFinal balancoFinal = new BalancoFinal(pedidos);
         System.out.println("\n=== Balanço Final do Restaurante ===");
         balancoFinal.exibirBalanco();
